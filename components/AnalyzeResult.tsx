69ff673d0f47fa762d22fd10e41726e48c6e59dd
YDG <zzmn1234@naver.com>
2024-04-09 01:28:36 +0900

    feat: 결과 뒤로가기 추가

--- components/AnalyzeResult.tsx
+++ components/AnalyzeResult.tsx
@@ -1,17 +1,23 @@
 "use client";
 import { coinNameMap } from "@/constants/coinNameMap";
 import { useCoinResultStore, useTotalAmount } from "@/store/result";
+import { useRouter } from "next/navigation";
 
 const AnalyzeResult = () => {
   const result = useCoinResultStore((state) => state.result);
   const total = useTotalAmount();
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.back();
+  };
   return (
-    <div className="flex w-64 flex-col gap-2 rounded-lg border border-black bg-slate-200 px-4 shadow-lg">
+    <div className="flex w-96 flex-col gap-2 rounded-lg border border-black bg-slate-100 px-4 shadow-lg">
       {result?.map((coin) => (
         <div key={coin.name} className="flex justify-between">
           <h3
             className={`font-bold ${
-              coin.amount < 0 ? "text-red-500" : "text-sky-400"
+              coin.amount < 0 ? "text-red-500" : "text-sky-500"
             }`}
           >
             {coinNameMap[coin.name]} {coin.name}
@@ -21,6 +27,7 @@ const AnalyzeResult = () => {
       ))}
 
       <p className="w-full text-right font-bold">총 {total}원</p>
+      <button onClick={handleClick}>뒤로가기</button>
     </div>
   );
 };
